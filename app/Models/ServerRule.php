f099f75a040a5d602d8c7aff8abddedacaf3bfaa
SunGreatSage <greatsage@keemail.me>
2025-03-17 14:39:27 +0800

    新增4. 节点组下发 model

---
+++ app/Models/ServerRule.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ServerRule extends Model
+{
+    protected $table = 'v2_rule';
+    protected $guarded = ['id'];
+    protected $casts = [
+        'created_at' => 'timestamp',
+        'updated_at' => 'timestamp'
+    ];
+}
