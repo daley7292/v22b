c974e32f703c9ab6db20147cfb9db10caf89f88b
daley7292 <daley7292@gmail.com>
2025-04-23 15:35:14 +0800

    replace mail template

--- resources/views/mail/default/notify.blade.php
+++ resources/views/mail/default/notify.blade.php
@@ -7,9 +7,7 @@
                     <table width="100%" border="0" cellspacing="0" cellpadding="0">
                         <thead>
                         <tr>
-                            <td valign="middle" style="padding-left:30px;background-color:#13484C;color:#fff;font-size: 36px;font-weight:700;line-height:64px">
-                                青瓦
-                            </td>
+                            <td valign="middle" style="padding-left:30px;background-color:#415A94;color:#fff;padding:20px 40px;font-size: 21px;">{{$name}}</td>
                         </tr>
                         </thead>
                         <tbody>
@@ -24,18 +22,7 @@
                                 {!! nl2br($content) !!}
                             </td>
                         </tr>
-                        <tr style="padding:20px;display:table-cell">
-                        </tr>
-                        <tr
-                            style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
-                            <td class="content-block"
-                                style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; text-align: center; vertical-align: top; margin: 0; padding: 0 0 20px;"
-                                valign="top">
-                                <a href="https://qw-ble.02000.net" class="btn-primary"
-                                    style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; color: #fff; text-decoration: none; line-height: 2em; font-weight: bold; text-align: center; cursor: pointer; display: inline-block; border-radius: 5px; text-transform: capitalize; background-color: #13484c; margin: 0; border-color: #13484c; border-style: solid; border-width: 8px 20px;">登录</a>
-                            </td>
-                        </tr>
-                        <tr style="padding:20px;display:table-cell">
+                        <tr style="padding:40px;display:table-cell">
                         </tr>
                         </tbody>
                     </table>
@@ -44,7 +31,7 @@
                     <table width="100%" border="0" cellspacing="0" cellpadding="0">
                         <tbody>
                         <tr>
-                            <td style="padding:20px 40px;font-size:12px;color:#999;line-height:20px;background:#13484C"><a href="https://www.bluetile.biz" target="blank" style="display:inline-block;width:100%;;font-size:14px;color:#fff;text-align:center;">官方唯一永久发布页：www.bluetile.biz</a></td>
+                            <td style="padding:20px 40px;font-size:12px;color:#999;line-height:20px;background:#f7f7f7"><a href="{{$url}}" style="font-size:14px;color:#929292">返回{{$name}}</a></td>
                         </tr>
                         </tbody>
                     </table>
