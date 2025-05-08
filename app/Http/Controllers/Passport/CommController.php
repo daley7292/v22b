217edfbf6d51103ad651a4c06e9b63beb15e68fe
daley7292 <daley7292@gmail.com>
2025-05-08 10:05:42 +0800

    Update CommController.php

--- app/Http/Controllers/Passport/CommController.php
+++ app/Http/Controllers/Passport/CommController.php
@@ -38,7 +38,7 @@ class CommController extends Controller
             abort(500, __('Email verification code has been sent, please request again later'));
         }
         $code = rand(100000, 999999);
-        $subject = config('v2board.app_name', 'V2Board') . __('Email verification code') . ':'.$code;;
+        $subject = config('v2board.app_name', 'V2Board') . ' ' . '官网:' . config('v2board.app_url', '') . ' ' . __('Email verification code') . ':'.$code;
 
         SendEmailJob::dispatch([
             'email' => $email,
